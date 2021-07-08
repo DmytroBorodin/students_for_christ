cb2593cafd4bcbff8cd834c29dc68c86e6f03e02
DmytroBorodin <dimabor96@gmail.com>
2021-07-09 00:23:32 +0300

    recources page

--- map.js
+++ map.js
@@ -46,24 +46,113 @@ series1.include = ids;
 // series1.mapPolygons.template.tooltipText = `{name} : {groupName}`;
 series1.mapPolygons.template.fill = gradient;
 series1.fill = gradient;
+series1.mapPolygons.template.cursorOverStyle = am4core.MouseCursorStyle.pointer;
 series1.data = [
   {
-    id: "FI",
-    flagURL: "./imgs/flag_fi.jpg",
+    id: "AL",
+    countryName: "Albania",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1525280882386-VRF32Z95VZKOYMKS5XQ8/Albaniaflag.gif?format=300w",
     tooltipTemplate: `
+    <h2 class="country__name__title">Albania</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="https://www.luminacampus.fi/" class="inline__link">Lumina Campus (Jyväskylä)</a></p>
+            <p class="tooltip__text">Name: S4C Durrës</p>
         </div>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="https://www.luminacampus.fi/" class="inline__link">Miika</a> & <a href="https://www.luminacampus.fi/" class="inline__link">Merja</a> Toukola</p>
-        </div>`,
+            <p class="tooltip__text"><a href="mailto:Elihu@live.it" class="inline__link">Emanuele Mango</a>, <a href="mailto:denymazz@gmail.com" class="inline__link">Denise Mazzoleni</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "AT",
+    countryName: "Austria",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484143722770-4E0UJCCKJ1I3ZCRSLQ3I/218px-Flag_of_Austria.svg.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Austria</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:alan.ambrose@agmd.org" class="inline__link">Alan & Michelle Ambrose</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text">Vienna Christian Center Young Adults</p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:austin.henderson@ma.agmd.org" class="inline__link">Austin & Tsehaynesh Henderson</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "BE",
+    countryName: "Belgium",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1487853185540-78EA1E5X3ZFC21I6KI59/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Belgium</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text"><a href="http://www.sfcbelgium.net/" class="inline__link">SfC Belgium</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="http://www.sfcbelgium.net" class="inline__link">Annayiz Goutier</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "BA",
+    countryName: "Bosnia and Herzegovina",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1586535679549-RI5T4L8ON21D11RPG6SA/bosnia-herzegovina.jpg?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Bosnia and Herzegovina</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:curthobbs@gmail.com" class="inline__link">Curt and Sara Hobbs</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "HR",
+    countryName: "Croatia",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1525290308678-G1BLCRONWKULETN688PC/croatia.jpg?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Croatia</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:stephen.turley@agmd.org" class="inline__link">Steve</a> & <a href=href="mailto:deanne.turley@agmd.org" class="inline__link">Daenan</a> Turley</p>
+        </div>
+        `,
+  },
+  {
+    id: "CZ",
+    countryName: "Czech Republic",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484143876575-K24T9IOZJ8NJKUOTT475/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Czech Republic</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text">Nová Generace</p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:posp.pavel@gmail.com" class="inline__link">Pavel & Kristina Pospisil</a></p>
+        </div>
+        `,
   },
   {
     id: "DK",
-    flagURL: "./imgs/flag_dk.png",
+    countryName: "Denmark",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484144364202-HJIV0Z6TCEKXA8I6X9MP/Flag_of_Denmark.svg.png?format=300w",
     tooltipTemplate: `
+        <h2 class="country__name__title">Denmark</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
             <p class="tooltip__text">Crossroads KBH</p>
@@ -75,23 +164,53 @@ series1.data = [
         `,
   },
   {
-    id: "SE",
-    flagURL: "./imgs/flag_se.png",
+    id: "FI",
+    countryName: "Finland",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1521453493361-56Q9RFUAQ4OPWHI8NFUF/finish.png?format=300w",
     tooltipTemplate: `
+        <h2 class="country__name__title">Finland</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="http://mosaikstudent.se/" class="inline__link">Mosaik</a></p>
+            <p class="tooltip__text"><a href="https://www.luminacampus.fi/" class="inline__link">Lumina Campus (Jyväskylä)</a></p>
         </div>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:mosaik@pingstung.se" class="inline__link">Brandon & Angela Koenes</a></p>
+            <p class="tooltip__text"><a href="https://www.luminacampus.fi/" class="inline__link">Miika</a> & <a href="https://www.luminacampus.fi/" class="inline__link">Merja</a> Toukola</p>
+        </div>`,
+  },
+  {
+    id: "FR",
+    countryName: "France",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484144394904-UC08ALMA6YB7LG8N1ITG/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">France</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text"><a href="https://remixetudiants.com" class="inline__link">Remix – Grenoble</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:remixgrenoble@gmail.com" class="inline__link">Mark & Dalene Good</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text"><a href="http://www.thebridgeparis.com/" class="inline__link">The Bridge – Paris</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:suzannespolarich@gmail.com" class="inline__link">Suzanne Spolarich</a></p>
         </div>
         `,
   },
   {
     id: "DE",
-    flagURL: "./imgs/flag_de.png",
+    countryName: "Germany",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1525280506576-A7IYKO9BOZAFUK64MP3Q/image-asset.png?format=300w",
     tooltipTemplate: `
+        <h2 class="country__name__title">Germany</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
             <p class="tooltip__text"><a href="http://www.studenten-fuer-christus.de" class="inline__link">Studenten für Christus</a></p>
@@ -103,9 +222,76 @@ series1.data = [
         `,
   },
   {
-    id: "NL",
-    flagURL: "./imgs/flag_nl.png",
+    id: "GR",
+    countryName: "Greece",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1525280527416-QHCZROHZMS2KVPYFIHAU/greekflag.jpg?format=300w",
     tooltipTemplate: `
+        <h2 class="country__name__title">Greece</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:briggsingreece@gmail.com" class="inline__link">Kennon & Amanda Briggs</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "HU",
+    countryName: "Hungary",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1487853259263-ONUC74RUJ3JJK44G1VBB/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Hungary</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text"><a href="https://www.facebook.com/riversideexit" class="inline__link">EXIT</a></p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:riversideexitgroup@gmail.com" class="inline__link">Elyse Hill</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "IE",
+    countryName: "Ireland",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1487853627329-QDGOM96BC4MXPXN8M407/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Ireland</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text">SfC Ireland</p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:naomi.r.carson@gmail.com" class="inline__link">Naomi Carson</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "IT",
+    countryName: "Italy",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484144834998-EXJV0ZKWBTX7YU7NOFC7/image-asset.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Italy</h2>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Group Name:</h3>
+            <p class="tooltip__text">SfC Italy</p>
+        </div>
+        <div class="tooltip__block">
+            <h3 class="tooltip__title">Contact Person:</h3>
+            <p class="tooltip__text"><a href="mailto:kurtis.denton@agmd.org" class="inline__link">Kurtis & Amy Denton</a></p>
+        </div>
+        `,
+  },
+  {
+    id: "NL",
+    countryName: "Netherlands",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484144933334-J9B0BD6KK6605OC1NQUK/218px-Flag_of_the_Netherlands.svg.png?format=300w",
+    tooltipTemplate: `
+        <h2 class="country__name__title">Netherlands</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
             <p class="tooltip__text"><a href="http://damascusroadic.com/drum/" class="inline__link">DR:UM (Maastricht)</a></p>
@@ -133,45 +319,12 @@ series1.data = [
         `,
   },
   {
-    id: "BE",
-    flagURL: "./imgs/flag_be.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="http://www.sfcbelgium.net/" class="inline__link">SfC Belgium</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="http://www.sfcbelgium.net" class="inline__link">Annayiz Goutier</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "FR",
-    flagURL: "./imgs/flag_fr.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="https://remixetudiants.com" class="inline__link">Remix – Grenoble</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:remixgrenoble@gmail.com" class="inline__link">Mark & Dalene Good</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="http://www.thebridgeparis.com/" class="inline__link">The Bridge – Paris</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:suzannespolarich@gmail.com" class="inline__link">Suzanne Spolarich</a></p>
-        </div>
-        `,
-  },
-  {
     id: "ES",
-    flagURL: "./imgs/flag_es.png",
+    countryName: "Spain",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484145100361-ZVTVD2EI4LC37Q1Y10E3/image-asset.png?format=300w",
     tooltipTemplate: `
+        <h2 class="country__name__title">Spain</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
             <p class="tooltip__text"><a href="http://raicesgranada.org/" class="inline__link">Raíces-Granada</a></p>
@@ -183,120 +336,19 @@ series1.data = [
         `,
   },
   {
-    id: "HR",
-    flagURL: "./imgs/flag_hr.jpg",
+    id: "SE",
+    countryName: "Sweden",
+    flagURL:
+      "https://images.squarespace-cdn.com/content/v1/5873d0bcb3db2b7d5e37770e/1484145158522-T41WKKDLKJW3QXOZ8N1R/image-asset.png?format=300w",
     tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:stephen.turley@agmd.org" class="inline__link">Steve</a> & <a href=href="mailto:deanne.turley@agmd.org" class="inline__link">Daenan</a> Turley</p>
-        </div>
-        `,
-  },
-  {
-    id: "IT",
-    flagURL: "./imgs/flag_it.png",
-    tooltipTemplate: `
+        <h2 class="country__name__title">Sweden</h2>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text">SfC Italy</p>
+            <p class="tooltip__text"><a href="http://mosaikstudent.se/" class="inline__link">Mosaik</a></p>
         </div>
         <div class="tooltip__block">
             <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:kurtis.denton@agmd.org" class="inline__link">Kurtis & Amy Denton</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "BA",
-    flagURL: "./imgs/flag_ba.jpg",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:curthobbs@gmail.com" class="inline__link">Curt and Sara Hobbs</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "HU",
-    flagURL: "./imgs/flag_hu.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text"><a href="https://www.facebook.com/riversideexit" class="inline__link">EXIT</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:riversideexitgroup@gmail.com" class="inline__link">Elyse Hill</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "AT",
-    flagURL: "./imgs/flag_at.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:alan.ambrose@agmd.org" class="inline__link">Alan & Michelle Ambrose</a></p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text">Vienna Christian Center Young Adults</p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:austin.henderson@ma.agmd.org" class="inline__link">Austin & Tsehaynesh Henderson</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "CZ",
-    flagURL: "./imgs/flag_cz.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text">Nová Generace</p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:posp.pavel@gmail.com" class="inline__link">Pavel & Kristina Pospisil</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "AL",
-    flagURL: "./imgs/flag_al.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text">Name: S4C Durrës</p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:Elihu@live.it" class="inline__link">Emanuele Mango</a>, <a href="mailto:denymazz@gmail.com" class="inline__link">Denise Mazzoleni</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "GR",
-    flagURL: "./imgs/flag_gr.jpg",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:briggsingreece@gmail.com" class="inline__link">Kennon & Amanda Briggs</a></p>
-        </div>
-        `,
-  },
-  {
-    id: "IE",
-    flagURL: "./imgs/flag_ie.png",
-    tooltipTemplate: `
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Group Name:</h3>
-            <p class="tooltip__text">SfC Ireland</p>
-        </div>
-        <div class="tooltip__block">
-            <h3 class="tooltip__title">Contact Person:</h3>
-            <p class="tooltip__text"><a href="mailto:naomi.r.carson@gmail.com" class="inline__link">Naomi Carson</a></p>
+            <p class="tooltip__text"><a href="mailto:mosaik@pingstung.se" class="inline__link">Brandon & Angela Koenes</a></p>
         </div>
         `,
   },
